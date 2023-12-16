e4a8d5a481968fe8b30a9de8b3ce6fed71f01853
NitionVR <kmaganyane83@gmail.com>
2023-12-17 00:53:32 +0200

    implemented the key_gen function

--- todoApp/todoApp.py
+++ todoApp/todoApp.py
@@ -10,6 +10,14 @@ def show_tasks(Tasks):
     print("\n")
 
 
+def key_gen(keys):
+    """Generates a unique key each time it is called"""
+    # if len(keys) == 1:
+    #     return 1
+    key = max(keys, default = 0) +1
+    return key
+
+
 
 
 
