9bf30fb3a1889461141926ed1067c9856f833cae
NitionVR <kmaganyane83@gmail.com>
2023-12-17 00:47:17 +0200

    implemented show_tasks and key_gen unittest methods

--- todoApp/tests/test_todoApp.py
+++ todoApp/tests/test_todoApp.py
@@ -20,27 +20,5 @@ class MyTestCase(unittest.TestCase):
         output = todoApp.key_gen({1,2,3})
         self.assertEqual(output,4)
 
-    #TODO: test for a case where the key is in the set
-    def test_update_tasks(self):
-        tasks = {100:"Build a to-do application", 200: "Turn it into an API"}
-        result = todoApp.update_tasks(tasks,{})
-        self.assertEqual(result,{1:"Build a to-do application", 2: "Turn it into an API"})
-
-    def test_remove_tasks(self):
-        tasks = {1:"Build a to-do application", 2: "Turn it into an API"}
-        result,keys = todoApp.remove_task(tasks,1,[1,2])
-        self.assertEqual(result,{1: "Turn it into an API"})
-
-    def test_get_tasks_description(self):
-        with captured_io(StringIO("Build a to-do application\n")) as (out,err):
-            result = todoApp.get_tasks_description()
-        self.assertEqual(result,"Build a to-do application")
-
-    def test_edit_task(self):
-        with captured_io(StringIO("Build a to-do application\n")) as (out,err):
-            result = todoApp.edit_task({1:"Build ato-doapplication", 2: "Turn it into an API"}\
-                                   ,1)
-        self.assertEqual(result,{1:"Build a to-do application", 2: "Turn it into an API"})
-        
 if __name__ == "__main__":
     unittest.main()
